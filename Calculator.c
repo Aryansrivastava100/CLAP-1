bc42ef17956356484ae83089e436faf165411cff
Aryansrivastava100 <103562253+Aryansrivastava100@users.noreply.github.com>
2022-04-23 23:18:31 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -3,11 +3,11 @@ int main()
 {
     int a,b,multiply,divide,sum,subtract;
     printf("Enter the value of A: ");
-    scanf("%d",&b);
-    printf("Enter the value of B: ");
     scanf("%d",&a);
+    printf("Enter the value of B: ");
+    scanf("%d",&b);
     sum=a+b;
-    subtract=b-a;
+    subtract=a-b;
     multiply=a*b;
     divide=a/b;
     printf("sum = %d\n",sum);
