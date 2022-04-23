4e99cc6d64326150ae71b13c44222f0d414d8890
Aryansrivastava100 <103562253+Aryansrivastava100@users.noreply.github.com>
2022-04-23 23:06:18 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,multiply,divide,sum,subtract;
+    printf("Enter the value of A: ");
+    scanf("%d",&b);
+    printf("Enter the value of B: ");
+    scanf("%d",&a);
+    sum=a+b;
+    subtract=b-a;
+    multiply=a*b;
+    divide=a/b;
+    printf("sum = %d\n",sum);
+    printf("subtract = %d\n",subtract);
+    printf("multiply = %d\n",multiply);
+    printf("divide = %d\n",divide);
+    return 0;
+} 
